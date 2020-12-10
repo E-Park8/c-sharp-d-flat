a2c76aa71740d1a4ac135ed1141d6f016a09e627
Eric Park <epark0995@gmail.com>
2020-12-09 17:45:19 -0800

    basic rock paper scissors function

--- Program.cs
+++ Program.cs
@@ -6,7 +6,77 @@ namespace c_sharp_d_flat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("C# Visual Studio Code Test");
+            string inputPlayer, inputCPU;
+            int randomInt;
+            int playerScore = 0;
+            int CPUScore = 0;
+
+            Console.Write("Choose between Rock, Paper, or Scissors:  ");
+            inputPlayer = Console.ReadLine();
+
+            Random rnd = new Random();
+
+            randomInt = rnd.Next(1, 4);
+
+            switch (randomInt)
+            {
+                case 1:
+                    inputCPU = "Rock";
+                    Console.WriteLine("Computer shoots Rock");
+                    if (inputPlayer == "Rock")
+                    {
+                        Console.WriteLine("Draw");
+                    }
+                    else if (inputPlayer == "Paper")
+                    {
+                        Console.WriteLine("Player Wins");
+                        playerScore++;
+                    }
+                    else if (inputPlayer == "Scissors")
+                    {
+                        Console.WriteLine("Computer Wins");
+                        CPUScore++;
+                    }
+                    break;
+                case 2:
+                    inputCPU = "Paper";
+                    Console.WriteLine("Computer shoots Paper");
+                    if (inputPlayer == "Rock")
+                    {
+                        Console.WriteLine("Computer Wins");
+                        CPUScore++;
+                    }
+                    else if (inputPlayer == "Paper")
+                    {
+                        Console.WriteLine("Draw");
+                    }
+                    else if (inputPlayer == "Scissors")
+                    {
+                        Console.WriteLine("Player Wins");
+                        playerScore++;
+                    }
+                    break;
+                case 3:
+                    inputCPU = "Scissors";
+                    Console.WriteLine("Computer shoots Scissors");
+                    if (inputPlayer == "Rock")
+                    {
+                        Console.WriteLine("Player Wins");
+                        playerScore++;
+                    }
+                    else if (inputPlayer == "Paper")
+                    {
+                        Console.WriteLine("Computer Wins");
+                        CPUScore++;
+                    }
+                    else if (inputPlayer == "Scissors")
+                    {
+                        Console.WriteLine("Draw");
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
