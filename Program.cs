d89083f539d890ae914f7d682ae84686925505ee
Eric Park <epark0995@gmail.com>
2020-12-09 17:55:12 -0800

    added scoring function

--- Program.cs
+++ Program.cs
@@ -8,74 +8,114 @@ namespace c_sharp_d_flat
         {
             string inputPlayer, inputCPU;
             int randomInt;
-            int playerScore = 0;
-            int CPUScore = 0;
 
-            Console.Write("Choose between Rock, Paper, or Scissors:  ");
-            inputPlayer = Console.ReadLine();
+            bool playAgain = true;
 
-            Random rnd = new Random();
-
-            randomInt = rnd.Next(1, 4);
-
-            switch (randomInt)
+            while (playAgain)
             {
-                case 1:
-                    inputCPU = "Rock";
-                    Console.WriteLine("Computer shoots Rock");
-                    if (inputPlayer == "Rock")
+                int playerScore = 0;
+                int CPUScore = 0;
+
+
+
+                while (playerScore < 3 && CPUScore < 3)
+                {
+
+
+                    Console.Write("Choose between Rock, Paper, or Scissors:  ");
+                    inputPlayer = Console.ReadLine();
+
+                    Random rnd = new Random();
+
+                    randomInt = rnd.Next(1, 4);
+
+                    switch (randomInt)
                     {
-                        Console.WriteLine("Draw");
+                        case 1:
+                            inputCPU = "Rock";
+                            Console.WriteLine("Computer shoots Rock");
+                            if (inputPlayer == "Rock")
+                            {
+                                Console.WriteLine("Draw");
+                            }
+                            else if (inputPlayer == "Paper")
+                            {
+                                Console.WriteLine("Player Wins");
+                                playerScore++;
+                            }
+                            else if (inputPlayer == "Scissors")
+                            {
+                                Console.WriteLine("Computer Wins");
+                                CPUScore++;
+                            }
+                            break;
+                        case 2:
+                            inputCPU = "Paper";
+                            Console.WriteLine("Computer shoots Paper");
+                            if (inputPlayer == "Rock")
+                            {
+                                Console.WriteLine("Computer Wins");
+                                CPUScore++;
+                            }
+                            else if (inputPlayer == "Paper")
+                            {
+                                Console.WriteLine("Draw");
+                            }
+                            else if (inputPlayer == "Scissors")
+                            {
+                                Console.WriteLine("Player Wins");
+                                playerScore++;
+                            }
+                            break;
+                        case 3:
+                            inputCPU = "Scissors";
+                            Console.WriteLine("Computer shoots Scissors");
+                            if (inputPlayer == "Rock")
+                            {
+                                Console.WriteLine("Player Wins");
+                                playerScore++;
+                            }
+                            else if (inputPlayer == "Paper")
+                            {
+                                Console.WriteLine("Computer Wins");
+                                CPUScore++;
+                            }
+                            else if (inputPlayer == "Scissors")
+                            {
+                                Console.WriteLine("Draw");
+                            }
+                            break;
+                        default:
+                            break;
                     }
-                    else if (inputPlayer == "Paper")
-                    {
-                        Console.WriteLine("Player Wins");
-                        playerScore++;
-                    }
-                    else if (inputPlayer == "Scissors")
-                    {
-                        Console.WriteLine("Computer Wins");
-                        CPUScore++;
-                    }
-                    break;
-                case 2:
-                    inputCPU = "Paper";
-                    Console.WriteLine("Computer shoots Paper");
-                    if (inputPlayer == "Rock")
-                    {
-                        Console.WriteLine("Computer Wins");
-                        CPUScore++;
-                    }
-                    else if (inputPlayer == "Paper")
-                    {
-                        Console.WriteLine("Draw");
-                    }
-                    else if (inputPlayer == "Scissors")
-                    {
-                        Console.WriteLine("Player Wins");
-                        playerScore++;
-                    }
-                    break;
-                case 3:
-                    inputCPU = "Scissors";
-                    Console.WriteLine("Computer shoots Scissors");
-                    if (inputPlayer == "Rock")
-                    {
-                        Console.WriteLine("Player Wins");
-                        playerScore++;
-                    }
-                    else if (inputPlayer == "Paper")
-                    {
-                        Console.WriteLine("Computer Wins");
-                        CPUScore++;
-                    }
-                    else if (inputPlayer == "Scissors")
-                    {
-                        Console.WriteLine("Draw");
-                    }
-                    break;
-                default:
-                    break;
+
+                }
+                if (playerScore == 3)
+                {
+                    Console.WriteLine("Player Won");
+                }
+                else if (CPUScore == 3)
+                {
+                    Console.WriteLine("Computer Won");
+                }
+                else
+                {
+
+                }
+                Console.WriteLine("Do you want to play again?(yes/no)");
+                string loop = Console.ReadLine();
+                if (loop == "yes")
+                {
+                    playAgain = true;
+                }
+                else if (loop == "no")
+                {
+                    playAgain = false;
+                }
+                else
+                {
+
+                }
             }
         }
     }
